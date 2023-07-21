025c8e932b03000eb272585caf998a478384a94c
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-07-22 00:01:37 +0200

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -1,31 +1,43 @@
-
-class EventFilter {
+ class EventFilter {
   constructor(availableKeywords) {
     this.availableKeywords = availableKeywords;
     this.resultsBox = document.querySelector(".result-box");
     this.inputBox = document.getElementById("input-box");
     this.inputBox.addEventListener("keyup", this.handleInput.bind(this));
     this.resultsBox.style.display = "none"; // Hide the results box initially
-
-    // Add a click event listener to the resultsBox to handle clicks on keywords
-    this.resultsBox.addEventListener("click", this.handleResultClick.bind(this));
   }
 
-  // ... other methods ...
+  handleInput() {
+    let input = this.inputBox.value;
+    let result = [];
+    if (input.length) {
+      result = this.availableKeywords.filter(keyword => {
+        return keyword.toLowerCase().includes(input.toLowerCase());
+      });
+    }
+
+    // Show or hide the results box based on the presence of filtered results
+    if (result.length > 0) {
+      this.display(result);
+      this.resultsBox.style.display = "block";
+    } else {
+      this.resultsBox.style.display = "none";
+    }
+  }
 
   display(results) {
     const content = results.map(list => {
-      return `<li><a href="#${list.toLowerCase().replace(/\s/g, "-")}">${list}</a></li>`;
+      return "<li onclick='this.selectInput(this)'>" + list + "</li>";
     });
 
-    this.resultsBox.innerHTML = `<ul>${content.join("")}</ul>`;
+    this.resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>";
   }
 
-  handleResultClick(event) {
-    const clickedKeyword = event.target.textContent;
-    this.inputBox.value = clickedKeyword;
+  selectInput(list) {
+    this.inputBox.value = list.innerHTML;
     this.resultsBox.style.display = "none"; // Hide the results box after selection
   }
-
-  // ... other methods ...
 }
+
+const availableKeywords = ["Paid events", "Free events"];
+const eventFilter = new EventFilter(availableKeywords);
