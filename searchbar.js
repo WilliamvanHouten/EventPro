58ba142a5dce6168cece4f9aca04a5e04a7ebb51
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-07-21 23:54:10 +0200

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -6,12 +6,25 @@ class EventFilter {
     this.inputBox = document.getElementById("input-box");
     this.inputBox.addEventListener("keyup", this.handleInput.bind(this));
     this.resultsBox.style.display = "none"; // Hide the results box initially
-
-    // Add a click event listener to the resultsBox to handle clicks on keywords
-    this.resultsBox.addEventListener("click", this.handleResultClick.bind(this));
   }
 
-  // ... other methods ...
+  handleInput() {
+    let input = this.inputBox.value;
+    let result = [];
+    if (input.length) {
+      result = this.availableKeywords.filter(keyword => {
+        return keyword.toLowerCase().includes(input.toLowerCase());
+      });
+    }
+
+    // Show or hide the results box based on the presence of filtered results
+    if (result.length > 0) {
+      this.display(result);
+      this.resultsBox.style.display = "block";
+    } else {
+      this.resultsBox.style.display = "none";
+    }
+  }
 
   display(results) {
     const content = results.map(list => {
@@ -21,11 +34,38 @@ class EventFilter {
     this.resultsBox.innerHTML = `<ul>${content.join("")}</ul>`;
   }
 
-  handleResultClick(event) {
-    const clickedKeyword = event.target.textContent;
-    this.inputBox.value = clickedKeyword;
+  selectInput(list) {
+    this.inputBox.value = list.innerHTML;
     this.resultsBox.style.display = "none"; // Hide the results box after selection
   }
-
-  // ... other methods ...
 }
+
+const availableKeywords = ["Paid events", "Free events"];
+const eventFilter = new EventFilter(availableKeywords);
+
+// Add a click event listener to the resultsBox to handle clicks on keywords
+eventFilter.resultsBox.addEventListener("click", event => {
+  if (event.target.tagName === "A") {
+    event.preventDefault(); // Prevent the default anchor link behavior
+    const clickedKeyword = event.target.textContent;
+    eventFilter.inputBox.value = clickedKeyword;
+    eventFilter.resultsBox.style.display = "none"; // Hide the results box after selection
+    const sectionId = clickedKeyword.toLowerCase().replace(/\s/g, "-");
+    scrollToSection(sectionId); // Scroll to the designated section
+  }
+});
+
+// Function to scroll to the designated section
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    const headerOffset = 100; // Adjust this value based on your header height
+    const elementPosition = section.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - headerOffset;
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  }
+}
+
