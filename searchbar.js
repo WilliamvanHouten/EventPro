18549107ece03cc378d0be3e7b028bc80d10e1a1
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-07-21 19:06:37 +0200

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -1,38 +1,44 @@
 
-    class EventFilter {
-      constructor(availableKeywords) {
-        this.availableKeywords = availableKeywords;
-        this.resultsBox = document.querySelector(".result-box");
-        this.inputBox = document.getElementById("input-box");
-        this.inputBox.addEventListener("keyup", this.handleInput.bind(this));
-      }
+ class EventFilter {
+  constructor(availableKeywords) {
+    this.availableKeywords = availableKeywords;
+    this.resultsBox = document.querySelector(".result-box");
+    this.inputBox = document.getElementById("input-box");
+    this.inputBox.addEventListener("keyup", this.handleInput.bind(this));
+    this.resultsBox.style.display = "none"; // Hide the results box initially
+  }
 
-      handleInput() {
-        let input = this.inputBox.value;
-        let result = [];
-        if (input.length) {
-          result = this.availableKeywords.filter(keyword => {
-            return keyword.toLowerCase().includes(input.toLowerCase());
-          });
-        }
-        this.display(result);
-      }
-
-      display(results) {
-        const content = results.map(list => {
-          return "<li onclick='this.selectInput(this)'>" + list + "</li>";
-        });
-
-        this.resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>";
-      }
-
-      selectInput(list) {
-        this.inputBox.value = list.innerHTML;
-        this.resultsBox.innerHTML = "";
-      }
+  handleInput() {
+    let input = this.inputBox.value;
+    let result = [];
+    if (input.length) {
+      result = this.availableKeywords.filter(keyword => {
+        return keyword.toLowerCase().includes(input.toLowerCase());
+      });
     }
 
-    const availableKeywords = ["Paid events", "Free events"];
-    const eventFilter = new EventFilter(availableKeywords);
+    // Show or hide the results box based on the presence of filtered results
+    if (result.length > 0) {
+      this.display(result);
+      this.resultsBox.style.display = "block";
+    } else {
+      this.resultsBox.style.display = "none";
+    }
+  }
 
+  display(results) {
+    const content = results.map(list => {
+      return "<li onclick='this.selectInput(this)'>" + list + "</li>";
+    });
 
+    this.resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+  }
+
+  selectInput(list) {
+    this.inputBox.value = list.innerHTML;
+    this.resultsBox.style.display = "none"; // Hide the results box after selection
+  }
+}
+
+const availableKeywords = ["Paid events", "Free events"];
+const eventFilter = new EventFilter(availableKeywords);
