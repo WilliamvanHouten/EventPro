41dd936e825de43f716b0e1aa2df90cf4e40a9f9
William <williamvnhtn@gmail.com>
2023-07-24 22:21:13 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,48 +1,30 @@
-// Create a TicketPurchase class
-class TicketPurchase {
-    constructor() {
-        this.eventName = '';
-        this.pricing = 0;
-        this.numTickets = 0;
+class EventCart {
+      constructor() {
+        this.cartItems = [];
+      }
+
+      addToCart(eventName, price, numberOfTickets) {
+        this.cartItems.push({
+          eventName: eventName,
+          price: price,
+          numberOfTickets: numberOfTickets
+        });
+        this.updateCartButton();
+      }
+
+      updateCartButton() {
+        const cartButton = document.getElementById("cartButton");
+        const cartTotal = this.cartItems.reduce((total, item) => total + item.numberOfTickets, 0);
+        cartButton.innerText = `Cart (${cartTotal})`;
+      }
     }
 
-    updateEventDetails(eventName, pricing, numTickets) {
-        this.eventName = eventName;
-        this.pricing = pricing;
-        this.numTickets = numTickets;
+    // Create an instance of EventCart
+    const eventCart = new EventCart();
+
+    // Function called when Buy Ticket button is clicked
+    function addToCart(eventName, price, selectId) {
+      const selectElement = document.getElementById(selectId);
+      const numberOfTickets = parseInt(selectElement.value, 10);
+      eventCart.addToCart(eventName, price * numberOfTickets, numberOfTickets);
     }
-
-    addToCart() {
-        const cartButton = document.getElementById('cartButton');
-        const cartAmount = this.numTickets * this.pricing;
-
-        // Update the cart button with the correct amount of tickets
-        cartButton.innerText = `Cart (${this.numTickets} Ticket(s) - R${cartAmount})`;
-
-        // Save the event details to local storage or any other method you prefer
-        localStorage.setItem('eventDetails', JSON.stringify({
-            eventName: this.eventName,
-            pricing: this.pricing,
-            numTickets: this.numTickets,
-            cartAmount: cartAmount
-        }));
-    }
-}
-
-// Function to handle Buy Ticket button click
-function addToCart() {
-    const eventName = "AI and Machine Learning Webinar-Sandton Convention Centre with Takalani Madzhadzhi";
-    const pricing = 380;
-    const numTicketsSelect = document.getElementById('early-bird-person');
-    const numTickets = parseInt(numTicketsSelect.value, 10);
-
-    // Create a TicketPurchase object
-    const ticketPurchase = new TicketPurchase();
-
-    // Update event details and add to cart
-    ticketPurchase.updateEventDetails(eventName, pricing, numTickets);
-    ticketPurchase.addToCart();
-
-    // Redirect to payment form page
-    window.location.href = "paymentform.html";
-}
