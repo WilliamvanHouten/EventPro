7382f5c9679948bf41dc227a43f4bf475b10f0c6
William <williamvnhtn@gmail.com>
2023-07-24 22:05:46 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,89 +1,48 @@
-// Define the EventCart class
-    class EventCart {
-      constructor() {
-        this.cart = [];
-      }
-
-      addToCart(event, quantity) {
-        this.cart.push({ event, quantity });
-        this.updateCartUI();
-      }
-
-      updateCartUI() {
-        // Update the cart UI here
-        // This function will be responsible for showing the current items in the cart and their quantities on the page
-        // You can use DOM manipulation to achieve this
-      }
+// Create a TicketPurchase class
+class TicketPurchase {
+    constructor() {
+        this.eventName = '';
+        this.pricing = 0;
+        this.numTickets = 0;
     }
 
-    // Create an instance of the EventCart class
-    const eventCart = new EventCart();
-
-    // Function to handle buy/booking button click
-    function handleButtonClick(event, quantity, isPaidEvent) {
-      // Check if the event is a paid event or a free event
-      if (isPaidEvent) {
-        // Perform the logic for paid events here
-        // Calculate the total amount based on the event price and quantity
-        const totalPrice = event.price * quantity;
-        // Add the event and quantity to the cart
-        eventCart.addToCart(event.name, quantity);
-        // Display a confirmation or update the UI to show the total price and quantity added to the cart
-      } else {
-        // Perform the logic for free events here
-        // Add the event and quantity to the cart
-        eventCart.addToCart(event.name, quantity);
-        // Display a confirmation or update the UI to show the quantity added to the cart
-      }
+    updateEventDetails(eventName, pricing, numTickets) {
+        this.eventName = eventName;
+        this.pricing = pricing;
+        this.numTickets = numTickets;
     }
 
-    // Function to handle the buy ticket button click
-    function handleBuyTicket(event) {
-      // Find the selected event and quantity from the DOM
-      const selectedEvent = event.target.closest('.pricing-box');
-      const eventName = selectedEvent.querySelector('.pricing-title h3').textContent;
-      const quantitySelect = selectedEvent.querySelector('.pricing-persons select');
-      const quantity = parseInt(quantitySelect.value);
+    addToCart() {
+        const cartButton = document.getElementById('cartButton');
+        const cartAmount = this.numTickets * this.pricing;
 
-      // For paid events, you should have a way to get the price of the event from the DOM
-      const price = ... // Get the price of the event from the DOM
+        // Update the cart button with the correct amount of tickets
+        cartButton.innerText = `Cart (${this.numTickets} Ticket(s) - R${cartAmount})`;
 
-      // Create an object representing the selected event
-      const selectedPaidEvent = {
-        name: eventName,
-        price: price,
-      };
-
-      // Call the handleButtonClick function with the necessary arguments
-      handleButtonClick(selectedPaidEvent, quantity, true);
+        // Save the event details to local storage or any other method you prefer
+        localStorage.setItem('eventDetails', JSON.stringify({
+            eventName: this.eventName,
+            pricing: this.pricing,
+            numTickets: this.numTickets,
+            cartAmount: cartAmount
+        }));
     }
+}
 
-    // Function to handle the book seat button click for free events
-    function handleBookSeat(event) {
-      // Find the selected event and quantity from the DOM
-      const selectedEvent = event.target.closest('.pricing-box');
-      const eventName = selectedEvent.querySelector('.pricing-title h3').textContent;
-      const quantitySelect = selectedEvent.querySelector('.pricing-persons select');
-      const quantity = parseInt(quantitySelect.value);
+// Function to handle Buy Ticket button click
+function addToCart() {
+    const eventName = "AI and Machine Learning Webinar-Sandton Convention Centre with Takalani Madzhadzhi";
+    const pricing = 380;
+    const numTicketsSelect = document.getElementById('early-bird-person');
+    const numTickets = parseInt(numTicketsSelect.value, 10);
 
-      // Create an object representing the selected event
-      const selectedFreeEvent = {
-        name: eventName,
-      };
+    // Create a TicketPurchase object
+    const ticketPurchase = new TicketPurchase();
 
-      // Call the handleButtonClick function with the necessary arguments
-      handleButtonClick(selectedFreeEvent, quantity, false);
-    }
+    // Update event details and add to cart
+    ticketPurchase.updateEventDetails(eventName, pricing, numTickets);
+    ticketPurchase.addToCart();
 
-    // Add event listeners to Buy Ticket buttons
-    const buyTicketButtons = document.querySelectorAll('#paid-events .pricing-box .pricing-action a');
-    buyTicketButtons.forEach(button => {
-      button.addEventListener('click', handleBuyTicket);
-    });
-
-    // Add event listeners to Book Seat buttons for free events
-    const bookSeatButtons = document.querySelectorAll('#free-events .pricing-box .pricing-action a');
-    bookSeatButtons.forEach(button => {
-      button.addEventListener('click', handleBookSeat);
-    });
-  
+    // Redirect to payment form page
+    window.location.href = "paymentform.html";
+}
