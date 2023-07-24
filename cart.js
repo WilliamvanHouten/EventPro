6ea7a757a540b8a55039ec633873e6a6fadf0a09
William <williamvnhtn@gmail.com>
2023-07-24 22:33:16 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -28,3 +28,83 @@ class EventCart {
       const numberOfTickets = parseInt(selectElement.value, 10);
       eventCart.addToCart(eventName, price * numberOfTickets, numberOfTickets);
     }
+
+// Format Card Number
+        function formatCardNumber() {
+            let cardNumberInput = document.getElementById('cardno');
+            let cardNumberValue = cardNumberInput.value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+            let formattedCardNumber = '';
+
+            if (cardNumberValue.length > 0) {
+                formattedCardNumber = cardNumberValue.match(/.{1,4}/g).join(' ');
+            }
+
+            cardNumberInput.value = formattedCardNumber;
+        }
+
+        // Format Expiration Date
+        function formatExpirationDate() {
+            let validTillInput = document.getElementById('validtill');
+            let validTillValue = validTillInput.value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+            let formattedValidTill = '';
+
+            if (validTillValue.length > 0) {
+                formattedValidTill = validTillValue.match(/.{1,2}/g).join('/');
+            }
+
+            validTillInput.value = formattedValidTill;
+        }
+
+        // Validate Cardholder Name
+        function validateCardholderName() {
+            let cardholderInput = document.getElementById('cardholder');
+            let cardholderValue = cardholderInput.value.trim();
+            let cardholderRegex = /^[a-zA-Z\s]+$/;
+
+            if (!cardholderRegex.test(cardholderValue)) {
+                cardholderInput.classList.add('is-invalid');
+            } else {
+                cardholderInput.classList.remove('is-invalid');
+            }
+        }
+
+        // Validate Form
+        function validateForm() {
+            let form = document.getElementById('paymentForm');
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+            return form.checkValidity();
+        }
+
+        // Enable/Disable Payment Button
+        function togglePaymentButton() {
+            let payButton = document.getElementById('payButton');
+            let isValid = validateForm();
+            payButton.disabled = !isValid;
+        }
+
+        // Show Confirmation Modal
+        function showConfirmationModal() {
+            $('#confirmationModal').modal('show');
+        }
+
+        // Close Confirmation Modal
+        function closeConfirmationModal() {
+            $('#confirmationModal').modal('hide');
+        }
+
+        // Event Listeners
+        document.getElementById('cardno').addEventListener('input', formatCardNumber);
+        document.getElementById('validtill').addEventListener('input', formatExpirationDate);
+        document.getElementById('cardholder').addEventListener('input', validateCardholderName);
+        document.getElementById('paymentForm').addEventListener('input', togglePaymentButton);
+        document.getElementById('paymentForm').addEventListener('submit', function (event) {
+            if (validateForm()) {
+                event.preventDefault();
+                showConfirmationModal();
+            }
+        });
+
